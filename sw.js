6954c0cbb3cdb982926d8030cf15d55c3176d556
mhtabmjawry-web <mhtabmjawry@gmail.com>
2026-06-05 07:36:35 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'clipnote-v3';
+const urls = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
